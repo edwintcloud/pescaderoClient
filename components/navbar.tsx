a485139cb9e3c7cc70b941c87805c62614b16526
Edwin Cloud <ecloud412@gmail.com>
2018-12-01 20:13:26 -0800

    save

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,7 +4,7 @@ export const NavBar = props => (
   <Menu secondary className="navbar">
     <Menu.Item header>Project Pescadero</Menu.Item>
     {props.user && (
-      <Dropdown icon simple className="right" direction="left" trigger={(<Image avatar src={props.user.avatar} />)}>
+      <Dropdown icon={null} simple className="right" direction="left" trigger={(<Image avatar src={props.user.avatar} />)}>
       <Dropdown.Menu>
         <Dropdown.Header><Image avatar src={props.user.avatar} /><span>{props.user.firstName}</span></Dropdown.Header>
         <DropdownDivider />
